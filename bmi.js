39d3266e97207957af4f1662f60a408fbd2a42d4
aditya sharma <adityasharma42072@gmail.com>
2023-03-07 13:00:27 +0530

    disappering display container

--- bmi.js
+++ bmi.js
@@ -14,41 +14,46 @@ let bmiInt = "helthy";
 
 
 btn.addEventListener('click' ,function() {
-    if(Iname.value == 'ujjwal'){
+    if(Iname.value == 'ujjwal' ){
     print.innerText = ` This is only for humans`;
     health.innerText = `khu khu kr krkr rkr krkr krk`;
     return
     }
-    // if(Iname.value === ""){
-    //     alert('enter a valid name');
-    // }
-    // if(Iweight.value === ""){
-    //     alert('enter valid weight');
-    // }
-    // if(Iheight.value === ""){
-    //     alert('enter valid height');
-    // }
+    if(Iname.value === ""){
+        alert('enter a valid name');
+    }else
+    if(Iheight.value === ""){
+        alert('enter valid height');
+    }else
+    if(Iweight.value === ""){
+        alert('enter valid weight');
+    }
     h = Iheight.value/100;
     let bmi = (Iweight.value/(h*h)).toFixed(2);
     if(bmi < 18.50){
         bmiInt = "underweight";
+        health.style.backgroundColor = "rgb(146, 146, 197)";
     }
     if(bmi > 18.50 && bmi < 24.90 ){
         bmiInt = "normal weight";
-    }
+        health.style.backgroundColor = "rgb(69, 206, 81)";
+        }
     if(bmi >= 25.00 && bmi <= 29.9 ){
         bmiInt = "overweight";
+        health.style.backgroundColor = "rgb(199, 113, 113)";
     }
     if(bmi >= 30.00 && bmi <= 35.00 ){
         bmiInt = "obese";
+        health.style.backgroundColor = "rgb(213, 33, 33)";
     }
     if(bmi > 35.00){
         bmiInt = "morbid obesity";
+        health.style.backgroundColor = "rgb(38, 23, 23)";
     }
   
     
     print.innerText = `${Iname.value} your bmi is ${bmi}`;
-    health.innerText = `you are ${bmiInt}`;
-    
+    health.innerText = `${bmiInt}`;
+    document.querySelector(".display").style.display = "block";
 });
 
